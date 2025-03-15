683d19af76d42fe3926235a6ed07c23ea0b59f0e
Oryxel <172038521+Oryxel@users.noreply.github.com>
2025-03-15 20:36:39 +0700

    Initial commit.

---
+++ src/main/java/org/oryxel/viabedrockpack/ViaBedrockPack.java
@@ -0,0 +1,15 @@
+package org.oryxel.viabedrockpack;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ViaBedrockPack implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("viabedrockpack");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Via!");
+	}
+}
